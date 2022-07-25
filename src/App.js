66876d0ced318002ba6d3903cc1bf3e0ae91786d
SUNNY-LAPTOP\Sunny <lovysunny7@gmail.com>
2022-07-25 22:48:01 +0900

    Add empty files to make lough trees

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Form from './components/form/form';
+import Layout from './components/layout/Layout';
+import List from './components/list/List';
+import Todo from './components/todo/Todo';
+import TodoList from './pages/TodoList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Header/>
       </header>
+      <Form/>
+      <Layout/>
+      <List/>
+      <Todo/>
+      <TodoList/>
     </div>
   );
 }
